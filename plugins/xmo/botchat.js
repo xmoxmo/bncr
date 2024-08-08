2fe07b640eb1a239fea3d20a153f733eb4e7b00d
xmo <112841961+xmoxmo@users.noreply.github.com>
2024-08-09 04:05:43 +0800

    Update botchat.js

--- plugins/xmo/botchat.js
+++ plugins/xmo/botchat.js
@@ -2,7 +2,7 @@
  * @author xmo
  * @name botchat
  * @team xmo
- * @version 2.4.8
+ * @version 2.4.9
  * @description 自动回复插件，可调用聊天插件如ChatGPT等回复，仅支持文本。
  * @rule ^(botreply)\s+(\S+)\s+([\s\S]+)$
  * @rule ^(botreply)\s+(\S+)\s+(del)$
@@ -23,8 +23,8 @@ const jsonSchema = BncrCreateSchema.object({
     enable: BncrCreateSchema.boolean().setTitle('指令开关').setDescription(`开启将启用匹配其他插件指令，开启并填写指令关键词后生效。`).setDefault(true),
     forward: BncrCreateSchema.string().setTitle('指令关键词').setDescription(`请输入其他插件匹配指令关键词，留空则不启用调用，仅读取数据库内容。`).setDefault('aigptv2'),
     forwardchat: BncrCreateSchema.string().setTitle('聊天模式指令关键词').setDescription(`为聊天模式单独设置其他插件匹配指令关键词，留空则使用"指令关键词"。`).setDefault(''),
+    replychat: BncrCreateSchema.array(BncrCreateSchema.string()).setTitle('禁用聊天模式的适配器').setDescription(`填写数据库中无匹配数据时不再调用"指令关键词"进行额外回复的适配器名称。`).setDefault([]),
     noname: BncrCreateSchema.array(BncrCreateSchema.string()).setTitle('无需设置bot名称的适配器').setDescription(`填写未设置bot名称不提示引导操作的适配器名称，设置bot名称主要用来识别群组内是否被@。`).setDefault(['web', 'ssh']),
-    outreply: BncrCreateSchema.array(BncrCreateSchema.string()).setTitle('禁用聊天模式的适配器').setDescription(`填写数据库中无匹配数据时不再调用"指令关键词"进行额外回复的适配器名称。`).setDefault([]),
   }).setTitle('基本设置').setDefault({}),
   debug: BncrCreateSchema.object({
     enable: BncrCreateSchema.boolean().setTitle('调试开关').setDescription(`开启将开启调试模式，对应平台管理员将收到额外的调试信息。`).setDefault(false),
@@ -45,7 +45,7 @@ module.exports = async (s) => {
     forwardlinechat = ConfigDB.userConfig.basic.forwardchat;
   }
   const nonamearr = ConfigDB.userConfig.basic.noname;
-  const outreplyarr = ConfigDB.userConfig.basic.outreply;
+  const replychatarr = ConfigDB.userConfig.basic.replychat;
   const sfrom = s.getFrom();
   const debug = ConfigDB.userConfig.debug.enable;
   const sysDB = new BncrDB('BotReplyDB');
@@ -233,7 +233,7 @@ module.exports = async (s) => {
       if (forwardlinechat) {
         forwardline = forwardlinechat;
       }
-      if (outreplyarr.indexOf(sfrom) != -1) {
+      if (replychatarr.indexOf(sfrom) != -1) {
         forwardline = '';
       }
       if (!groupId || groupId === '0') {
