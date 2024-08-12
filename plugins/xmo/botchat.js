410d56153bfe309015ff05af99a33d4cf6a1d5c6
xmo <112841961+xmoxmo@users.noreply.github.com>
2024-08-12 08:35:09 +0800

    Update botchat.js

--- plugins/xmo/botchat.js
+++ plugins/xmo/botchat.js
@@ -2,7 +2,7 @@
  * @author xmo
  * @name botchat
  * @team xmo
- * @version 2.5.4
+ * @version 2.5.5
  * @description 自动回复插件，可调用聊天插件如ChatGPT等回复，仅支持文本。
  * @rule ^(botreply)\s+(\S+)\s+([\s\S]+)$
  * @rule ^(botreply)\s+(\S+)\s+(del)$
@@ -426,6 +426,7 @@ module.exports = async (s) => {
             for (var k = 0; k < replydbs.length; k++) {
               await funsendreply(replydbs[k]);
             }
+            return "@noreply@"
           } else {
             return await funsendreply(replydb);
           }
