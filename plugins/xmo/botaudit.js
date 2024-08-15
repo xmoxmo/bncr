a8e7884d8c93f8c57af8b76023ef6400cdb382e5
xmo <112841961+xmoxmo@users.noreply.github.com>
2024-08-15 21:43:30 +0800

    Update botaudit.js

--- plugins/xmo/botaudit.js
+++ plugins/xmo/botaudit.js
@@ -2,7 +2,7 @@
  * @author xmo
  * @name botaudit
  * @team xmo
- * @version 1.2.5
+ * @version 1.2.6
  * @description 黑名单模式按平台、群组、用户屏蔽关键词响应。
  * @rule ^(botaudit)\s+(\S+)\s+([\s\S]+)$
  * @rule ^(botaudit)\s+(\S+)\s+(del)$
@@ -25,7 +25,7 @@ const jsonSchema = BncrCreateSchema.object({
     enable: BncrCreateSchema.boolean().setTitle('调试开关').setDescription(`开启将开启调试模式，对应平台管理员将收到额外的调试信息。`).setDefault(false),
   }).setTitle('调试设置').setDefault({})
 });
-const ver = '1.2.5';
+const ver = '1.2.6';
 const ConfigDB = new BncrPluginConfig(jsonSchema);
 module.exports = async (s) => {
   if (!Object.keys(ConfigDB.userConfig).length) {
@@ -321,6 +321,9 @@ module.exports = async (s) => {
             for (var i = 0; i < keys.length; i++) {
               let str = keys[i];
               if (!(await keycheck(str))) {
+                if (str.includes('*')) {
+                  str = str.replace(new RegExp('\*','g'), "");
+                }
                 if (keyword.includes(str)) {
                   newkeyword = keys[i];
                   break;
