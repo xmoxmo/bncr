be3145a7de84f7c227cc83c16b397f4142430849
xmo <112841961+xmoxmo@users.noreply.github.com>
2024-08-17 14:10:36 +0800

    Update botreply.js

--- plugins/xmo/botreply.js
+++ plugins/xmo/botreply.js
@@ -2,7 +2,7 @@
  * @author xmo
  * @name botreply
  * @team xmo
- * @version 2.8.3
+ * @version 2.8.4
  * @description 自动回复插件，可调用聊天插件如ChatGPT等回复，仅支持文本。
  * @rule ^(botreply)\s+(\S+)\s+([\s\S]+)$
  * @rule ^(botreply)\s+(\S+)\s+(del)$
@@ -28,7 +28,7 @@ const jsonSchema = BncrCreateSchema.object({
     enable: BncrCreateSchema.boolean().setTitle('调试开关').setDescription(`开启将开启调试模式，对应平台管理员将收到额外的调试信息。`).setDefault(false),
   }).setTitle('调试设置').setDefault({})
 });
-const ver = '2.8.3';
+const ver = '2.8.4';
 const ConfigDB = new BncrPluginConfig(jsonSchema);
 module.exports = async (s) => {
   if (!Object.keys(ConfigDB.userConfig).length) {
@@ -109,6 +109,11 @@ module.exports = async (s) => {
       return s.reply('你没有权限执行此操作');
     }
 
+    let keys = await sysDB.keys();
+    if (keys.indexOf(keyword) == -1) {
+      s.reply(`删除失败：数据库中无[${keyword}]`);
+      return null;
+    }
     const result = await deleteReply(keyword);
     // console.log(`Delete reply result for keyword ${keyword}: ${result}`);
     s.reply(result ? '删除成功' : '删除失败');
