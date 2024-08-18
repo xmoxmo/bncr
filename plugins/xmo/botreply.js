5566864d1ecce106bacbc7a6973038ce142fe066
xmo <112841961+xmoxmo@users.noreply.github.com>
2024-08-18 22:32:47 +0800

    Update botreply.js

--- plugins/xmo/botreply.js
+++ plugins/xmo/botreply.js
@@ -2,7 +2,7 @@
  * @author xmo
  * @name botreply
  * @team xmo
- * @version 2.8.8
+ * @version 2.8.9
  * @description 自动回复插件，可调用聊天插件如ChatGPT等回复，仅支持文本。
  * @rule ^(botreply)\s+(\S+)\s+([\s\S]+)$
  * @rule ^(botreply)\s+(\S+)\s+(del)$
@@ -28,7 +28,7 @@ const jsonSchema = BncrCreateSchema.object({
     enable: BncrCreateSchema.boolean().setTitle('调试开关').setDescription(`开启将开启调试模式，对应平台管理员将收到额外的调试信息。`).setDefault(false),
   }).setTitle('调试设置').setDefault({})
 });
-const ver = '2.8.8';
+const ver = '2.8.9';
 const ConfigDB = new BncrPluginConfig(jsonSchema);
 module.exports = async (s) => {
   if (!Object.keys(ConfigDB.userConfig).length) {
@@ -484,10 +484,10 @@ module.exports = async (s) => {
       if (keyword.slice(0, 7) === '@remsg@') {
         return '@noreply@';
       } else {
-        if keyword.includes(':') {
+        if (keyword.includes(':')) {
           let keywords = keyword.split(':');
           keyword = keywords[0];
-          userkeyword = keyword[1];
+          userkeyword = keywords[1];
         }
         let keys = await sysDB.keys();
         keys = await sortArray(keys);
