627efa42b5b648305e4f3709c4d2cd1870499686
xmo <henwuji007@163.com>
2024-08-21 10:53:10 +0800

    Update botreply.js

--- plugins/xmo/botreply.js
+++ plugins/xmo/botreply.js
@@ -2,7 +2,7 @@
  * @author xmo
  * @name botreply
  * @team xmo
- * @version 3.0.0
+ * @version 3.0.1
  * @description 自动回复插件，可调用聊天插件如ChatGPT等回复，仅支持文本。
  * @rule ^(botreply)\s+(\S+)\s+([\s\S]+)$
  * @rule ^(botreply)\s+(\S+)\s+(del)$
@@ -28,7 +28,7 @@ const jsonSchema = BncrCreateSchema.object({
     enable: BncrCreateSchema.boolean().setTitle('调试开关').setDescription(`开启将开启调试模式，对应平台管理员将收到额外的调试信息。`).setDefault(false),
   }).setTitle('调试设置').setDefault({})
 });
-const ver = '3.0.0';
+const ver = '3.0.1';
 const ConfigDB = new BncrPluginConfig(jsonSchema);
 module.exports = async (s) => {
   if (!Object.keys(ConfigDB.userConfig).length) {
@@ -324,6 +324,12 @@ module.exports = async (s) => {
         s.reply('你没有权限执行此操作');
       }
       return null;
+    }
+    if (forwardline) {
+      if (keyword.includes(forwardline)) {
+        s.reply('“指令关键词”未响应，请检查指令关键词是否正确');
+        return null;
+      }
     }
     if (keyword.includes('@group@')) {
       if (!(await s.isAdmin())) {
@@ -628,7 +634,7 @@ module.exports = async (s) => {
             if (replydb.slice(0, 7) === '@remsg@') {
               replydb = replydb.slice(7);
               if (replydb.includes('@chatcom@')) {
-                replydb = replydb.replace(new RegExp('@chatcom@','g'),forwardline);
+                replydb = replydb.replace(new RegExp('@chatcom@','g'), forwardline);
                 if (forwardline) {
                   s.inlineSugar(replydb);
                 }
