b567c9af8d895f42e4ad4a521f03b629d296008e
xmo <henwuji007@163.com>
2024-09-17 03:35:45 +0800

    Update wechatbot.js

--- Adapter/wechatbot.js
+++ Adapter/wechatbot.js
@@ -4,7 +4,7 @@
  * @name wechatbot
  * @origin xmo
  * @team xmo
- * @version 0.1.4
+ * @version 0.1.5
  * @description wechatbot适配器，项目地址：https://gitee.com/ilooli/wechat-bot
  * @adapter true
  * @public true
@@ -64,8 +64,8 @@ module.exports = async () => {
         if (body.type === 'SYSTEM') {
           const tips = body.content;
           if (tips.includes('加入了群聊')) {
-            sysMethod.startOutLogs(`wechatbot：收到群员进群事件`);
             const topic = body.from.NickName;
+            sysMethod.startOutLogs(`wechatbot：收到群员进群事件：${tips}>${topic}`);
             const tipss = tips.split(`"`);
             const users = tipss[3];
             const roomId = Buffer.from(topic, 'utf-8').toString('hex');
