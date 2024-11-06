88fcf0645b116d0370a000ee891129e513344824
xmo <henwuji007@163.com>
2024-11-06 13:08:35 +0800

    Update botreply.js

--- plugins/xmo/botreply.js
+++ plugins/xmo/botreply.js
@@ -2,7 +2,7 @@
  * @author xmo
  * @name botreply
  * @team xmo
- * @version 3.1.6
+ * @version 3.1.7
  * @description 自动回复插件，可调用聊天插件如ChatGPT等回复，仅支持文本。
  * @rule ^(botreply)\s+(\S+)\s+([\s\S]+)$
  * @rule ^(botreply)\s+(\S+)\s+(del)$
@@ -39,7 +39,7 @@ const jsonSchema = BncrCreateSchema.object({
     enable: BncrCreateSchema.boolean().setTitle('调试开关').setDescription(`开启将开启调试模式，对应平台管理员将收到额外的调试信息。`).setDefault(false),
   }).setTitle('调试设置').setDefault({})
 });
-const ver = '3.1.6';
+const ver = '3.1.7';
 const ConfigDB = new BncrPluginConfig(jsonSchema);
 module.exports = async (s) => {
   if (!Object.keys(ConfigDB.userConfig).length) {
@@ -87,7 +87,7 @@ module.exports = async (s) => {
   const getTime = sysMethod.getTime();
   const sysDB = new BncrDB('BotReplyDB');
   const commandType = s.param(1);
-  const keyword = s.param(2);
+  const keyword = decodeURIComponent(s.param(2));
   const replyContent = s.param(3);
 
   // console.log(`Received command: ${commandType}, Keyword: ${keyword}, ReplyContent: ${replyContent}`);
