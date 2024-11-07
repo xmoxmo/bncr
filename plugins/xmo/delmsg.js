26041b61f53652e199999533cd9993c8206f58a0
xmo <henwuji007@163.com>
2024-11-08 00:30:50 +0800

    Create delmsg.js

---
+++ plugins/xmo/delmsg.js
@@ -0,0 +1,28 @@
+/**
+ * @author xmo
+ * @name delmsg
+ * @team xmo
+ * @version 0.0.2
+ * @description 拓展人行tg功能
+ * @create_at 2024-11-08 19:27:24
+ * @rule ^(\.tgde) ([0-9]+) ([0-9]+)$
+ * @priority 1000
+ * @admin true
+ * @public false
+ * @disable false
+ * @classification []
+ */
+
+module.exports = async s => {
+  if (s.getFrom() !== 'HumanTG' || !(await s.isAdmin())) return;
+  const userId = +s.getUserId();
+  const groupId = +s.getGroupId();
+  const friendId = +s.msgInfo.friendId;
+  const ChatID = +groupId || +friendId || +userId;
+  let num = +s.param(2);
+  let delay = +s.param(3);
+  let info = '';
+  info= await s.Bridge.getUserMsgId(ChatID, s.getUserId(), num + 1);
+  info.length && s.delMsg(...info, { wait: delay });
+  return;
+};
