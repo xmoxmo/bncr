612993c6b3e1ddf17f12d2876b60681a47b4fcf3
xmo <henwuji007@163.com>
2024-11-08 17:35:11 +0800

    Update delmsg.js

--- plugins/xmo/delmsg.js
+++ plugins/xmo/delmsg.js
@@ -2,7 +2,7 @@
  * @author xmo
  * @name delmsg
  * @team xmo
- * @version 0.0.4
+ * @version 0.0.5
  * @description 拓展人行tg撤回消息功能
  * @create_at 2024-11-08 19:27:24
  * @rule ^(\.tgde) ([0-9]+) ([0-9]+)$
@@ -23,7 +23,7 @@ module.exports = async s => {
   let num = +s.param(2);
   let delay = +s.param(3) || 1;
   let info = '';
-  info= await s.Bridge.getUserMsgId(ChatID, s.getUserId(), num + 1);
+  info= await s.Bridge.getUserMsgId(ChatID, s.getUserId(), num);
   info.length && s.delMsg(...info, { wait: delay });
   return;
 };
