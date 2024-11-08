2bd52edb6d0051d3c44365ad012958dfccd7626c
xmo <henwuji007@163.com>
2024-11-08 17:37:30 +0800

    Update botreply.js

--- plugins/xmo/botreply.js
+++ plugins/xmo/botreply.js
@@ -2,7 +2,7 @@
  * @author xmo
  * @name botreply
  * @team xmo
- * @version 3.2.7
+ * @version 3.2.8
  * @description 自动回复插件，可调用聊天插件如ChatGPT等回复，仅支持文本。
  * @rule ^(botreply)\s+(\S+)\s+([\s\S]+)$
  * @rule ^(botreply)\s+(\S+)\s+(del)$
@@ -46,7 +46,7 @@ const jsonSchema = BncrCreateSchema.object({
     enable: BncrCreateSchema.boolean().setTitle('调试开关').setDescription(`开启将开启调试模式，对应平台管理员将收到额外的调试信息。`).setDefault(false),
   }).setTitle('调试设置').setDefault({})
 });
-const ver = '3.2.7';
+const ver = '3.2.8';
 const ConfigDB = new BncrPluginConfig(jsonSchema);
 module.exports = async (s) => {
   if (!Object.keys(ConfigDB.userConfig).length) {
@@ -892,6 +892,7 @@ module.exports = async (s) => {
               msg: chcmd,
               userId: botid || '0',
               groupId: groupId || '0',
+              friendId: userId || '0',
             }
             sysMethod.Adapters(msgInfo, sfrom, 'inlinemask', msgInfo);
           }
